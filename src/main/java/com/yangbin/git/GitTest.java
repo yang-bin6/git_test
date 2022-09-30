c621ba83fa1fe5da373a67d5ef8d8c1655567f76
yangbin <yangbin@YB.com>
2022-09-30 14:30:45 +0800

    push commit

--- src/main/java/com/yangbin/git/GitTest.java
+++ src/main/java/com/yangbin/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello yang-bin!");
         System.out.println("master text!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
